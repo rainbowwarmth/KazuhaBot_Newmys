4cf30c02a4a332f8cf1dbd18b9dc186b5a4b5104
WangQueXL <95076858+WangQueXL@users.noreply.github.com>
2023-12-10 17:26:11 +0800

    添加 详细更新日志 功能

--- src/plugins/help.ts
+++ src/plugins/help.ts
@@ -31,3 +31,56 @@ export async function version(msg: IMessageEx){
     })
 
 }
+
+interface Commit {
+    commit: {
+        author: {
+            name: string;
+            date: string;
+        };
+        message: string;
+    };
+    authorName: string;
+    authorDate: string;
+    commitMessage: string;
+}
+
+export async function commits(msg: IMessageEx) {
+    try {
+        const response = await fetch('https://gitee.com/api/v5/repos/rainbowwarmth/KazuhaBot_Newmys/commits');
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+
+        // 提取项目中的信息
+        const extractedData = data.slice(0, 15).map((commit: Commit) => {
+            const messageWithoutLinks = commit.commit.message.replace(/\./g, ' ');
+            return {
+                authorName: commit.commit.author.name,
+                authorDate: commit.commit.author.date,
+                commitMessage: messageWithoutLinks,
+            };
+        });
+        // 循环遍历提取的数据发送到控制台
+        extractedData.forEach((commit: Commit) => {
+            console.log('Author Name:', commit.authorName);
+            console.log('Author Date:', commit.authorDate);
+            console.log('Commit Message:', commit.commitMessage);
+            console.log('\n');
+        });
+
+        let content = '更新日志\n';
+        extractedData.forEach((commit: Commit) => {
+            content += `\n作者：${commit.authorName}\n时间：${commit.authorDate}\n内容：${commit.commitMessage}\n`;
+        });
+
+        return msg.sendMsgEx({
+            content: content
+        });
+
+    } catch (error) {
+        console.error('Error fetching or parsing data:', error);
+        return null;
+    }
+}
