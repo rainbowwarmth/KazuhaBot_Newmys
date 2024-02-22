b5cd6ad0d21e7203bddc4e183003565ed8c9b11c
rainbowwarmth <3445487890@qq.com>
2024-02-22 18:27:30 +0800

    优化

--- config/pm2/pm2-run.js
+++ config/pm2/pm2-run.js
@@ -1,2 +1,2 @@
 require('ts-node').register();
-require('./app.ts');
+require('../../app.ts');
--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -21,13 +21,19 @@ const log = log4js.configure({
 }).getLogger();
 
 export function setDevLog() {
-    log.setParseCallStackFunction((error: Error) => {
+    log.setParseCallStackFunction((error: Error, linesToSkip: number) => {
         const stacklines = error.stack?.split("\n")!.splice(4)!;
         const lineMatch = /at (?:(.+)\s+\()?(?:(.+?):(\d+)(?::(\d+))?|([^)]+))\)?/.exec(stacklines[0]);
         /* istanbul ignore else: failsafe */
         if (lineMatch && lineMatch.length === 6)
-            return { fileName: ` [${lineMatch[2].replace(`${_path}/`, "")}:${lineMatch[3]}:${lineMatch[4]}]` };
-    });
+            return { 
+        functionName: 'FunctionName',
+        lineNumber: 23,
+        columnNumber: 35,
+        callStack: 'CallStackInformation',
+        fileName: ` [${lineMatch[2].replace(`${_path}/`, "")}:${lineMatch[3]}:${lineMatch[4]}]` 
+    };
+});
 }
 
 export default log;
